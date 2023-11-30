469227e74f10599a8b6c577e709c544ac91abcaf
Arya Galuh Saputra <151036032+varrietty@users.noreply.github.com>
2023-11-30 20:42:55 +0700

    Update koneksi.php
    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE AND E_DEPRECATED);
-$host = 'link hosting';
-$user = 'username hosting';
-$pass = 'password hosting';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
 $db   = 'naruto';
 
 $koneksi = mysqli_connect($host,$user,$pass,$db) or die("Koneksi gagal!");
